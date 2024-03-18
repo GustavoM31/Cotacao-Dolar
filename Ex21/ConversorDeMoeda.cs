64b33bbde0e90d893e9a0c09f07d8a397aefe1dc
Gustavo Morais <gustavo.morais3119@gmail.com>
2024-03-18 17:41:59 -0300

    Add project files.

---
+++ Ex21/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex21
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * IOF/100;
+        }
+    }
+}
